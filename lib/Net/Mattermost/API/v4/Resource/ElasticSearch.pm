e382efa091ec4d1a61d355f484a3482624af2550
Mike Jones <mike@netsplit.org.uk>
2018-08-12 15:06:35 +0100

    ElasticSearch methods

--- lib/Net/Mattermost/API/v4/Resource/ElasticSearch.pm
+++ lib/Net/Mattermost/API/v4/Resource/ElasticSearch.pm
@@ -4,5 +4,75 @@ use Moo;
 
 extends 'Net::Mattermost::API::v4::Resource';
 
+################################################################################
+
+sub test {
+    my $self = shift;
+
+    return $self->_call({
+        method   => $self->post,
+        endpoint => 'test',
+    });
+}
+
+sub purge_indexes {
+    my $self = shift;
+
+    return $self->_call({
+        method   => $self->post,
+        endpoint => 'purge',
+    });
+}
+
+################################################################################
+
 1;
+__END__
+
+=head1 NAME
+
+Net::Mattermost::API::v4::Resource::ElasticSearch
+
+=head1 DESCRIPTION
+
+=head2 USAGE
+
+    use Net::Mattermost;
+
+    my $mm = Net::Mattermost->new({
+        authenticate => 1,
+        username     => 'email@address.com',
+        password     => 'passwordhere',
+        base_url     => 'https://my.mattermost.server.com/api/v4/',
+    });
+
+    my $elasticsearch = $mm->api->elasticsearch;
+
+=head2 METHODS
+
+=over 4
+
+=item C<test()>
+
+    my $response = $elasticsearch->test;
+
+=item C<purge_indexes()>
+
+    my $response = $elasticsearch->purge_indexes;
+
+=back
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<https://api.mattermost.com/#tag/elasticsearch>
+
+Official "ElasticSearch" API documentation.
+
+=back
+
+=head1 AUTHOR
+
+Mike Jones L<email:mike@netsplit.org.uk>
 
