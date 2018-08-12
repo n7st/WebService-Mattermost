1086d1c5dee97e4fd834182be8a51995d92ba3ee
Mike Jones <mike@netsplit.org.uk>
2018-08-12 15:03:33 +0100

    Add Cluster methods

--- lib/Net/Mattermost/API/v4/Resource/Cluster.pm
+++ lib/Net/Mattermost/API/v4/Resource/Cluster.pm
@@ -4,5 +4,62 @@ use Moo;
 
 extends 'Net::Mattermost::API::v4::Resource';
 
+################################################################################
+
+sub status {
+    my $self = shift;
+
+    return $self->_call({
+        method   => $self->get,
+        endpoint => 'status',
+    });
+}
+
+################################################################################
+
 1;
+__END__
+
+=head1 NAME
+
+Net::Mattermost::API::v4::Resource::Cluster
+
+=head1 DESCRIPTION
+
+=head2 USAGE
+
+    use Net::Mattermost;
+
+    my $mm = Net::Mattermost->new({
+        authenticate => 1,
+        username     => 'email@address.com',
+        password     => 'passwordhere',
+        base_url     => 'https://my.mattermost.server.com/api/v4/',
+    });
+
+    my $cluster = $mm->api->cluster;
+
+=head2 METHODS
+
+=over 4
+
+=item C<status()>
+
+    my $response = $cluster->status;
+
+=back
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<https://api.mattermost.com/#tag/cluster>
+
+Official "cluster" API documentation.
+
+=back
+
+=head1 AUTHOR
+
+Mike Jones L<email:mike@netsplit.org.uk>
 
