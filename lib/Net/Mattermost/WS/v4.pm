30988f6e1112b53cd976ea769e0bae18e3d22410
Mike Jones <mike@netsplit.org.uk>
2018-08-21 18:39:53 +0000

    Emit events from WebSocket integration class

--- lib/Net/Mattermost/WS/v4.pm
+++ lib/Net/Mattermost/WS/v4.pm
@@ -6,19 +6,24 @@ use Mojo::IOLoop;
 use Mojo::JSON qw(decode_json encode_json);
 use Moo;
 use MooX::HandlesVia;
-use Types::Standard qw(ArrayRef Bool InstanceOf Int Maybe Str);
+use Types::Standard qw(ArrayRef HashRef Bool InstanceOf Int Maybe Str);
 
-extends 'Net::Mattermost';
-with    qw(
+extends qw(
+    Net::Mattermost
+    Mojo::EventEmitter
+);
+
+with qw(
     Net::Mattermost::Role::Logger
     Net::Mattermost::Role::UserAgent
 );
 
 ################################################################################
 
+has _ua           => (is => 'rw', isa => InstanceOf['Mojo::UserAgent'], lazy => 1, builder => 1);
+has event_map     => (is => 'ro', isa => HashRef,                       lazy => 1, builder => 1);
 has ioloop        => (is => 'rw', isa => InstanceOf['Mojo::IOLoop'],    lazy => 1, builder => 1);
 has websocket_url => (is => 'ro', isa => Str,                           lazy => 1, builder => 1);
-has _ua           => (is => 'rw', isa => InstanceOf['Mojo::UserAgent'], lazy => 1, builder => 1);
 
 has ws => (is => 'rw', isa => Maybe[InstanceOf['Mojo::Base']]);
 
@@ -45,6 +50,17 @@ sub BUILD {
 
     $self->authenticate(1);
 
+    # Set up expected subroutines for a child class to catch. The events can
+    # also be caught raw in a script.
+    foreach my $kx (keys %{$self->event_map}) {
+        my $vx = $self->event_map->{$kx};
+
+        # Values from event_map must be set up in child class
+        if ($self->can($vx)) {
+            $self->on($kx, sub { shift; $self->$vx(@_) });
+        }
+    }
+
     return $self->next::method(@_);
 }
 
@@ -55,6 +71,14 @@ sub start {
     $self->ioloop->start unless $self->ioloop->is_running();
 
     return;
+}
+
+sub message_has_content {
+    my $self = shift;
+    my $args = shift;
+
+    return $args->{post_data}
+        && $args->{post_data}->{message};
 }
 
 ################################################################################
@@ -72,6 +96,8 @@ sub _connect {
         unless ($tx->is_websocket) {
             $self->logger->fatalf('WebSocket handshake failed: %s', $tx->res->error->{message});
         }
+
+        $self->emit(started => {});
 
         $self->logger->debug('Adding ping loop');
         $self->add_loop($self->ioloop->recurring(15 => sub { $self->_ping($tx) }));
@@ -126,6 +152,7 @@ sub _on_finish {
     $self->logger->infof('Reconnecting in %d seconds...', $self->reconnection_wait_time);
 
     $self->ws->finish;
+    $self->emit(finished => { code => $code, reason => $reason });
 
     # Delay the reconnection a little
     Mojo::IOLoop->timer($self->reconnection_wait_time => sub {
@@ -149,11 +176,13 @@ sub _on_message {
 
     return $self->_on_non_event($message) unless $message && $message->{event};
 
-    my $output;
+    my $message_args = { message => $message };
 
     #if ($self->ignore_self && $message->{data}->{post}) {
     if ($message->{data}->{post}) {
         my $post_data = decode_json($message->{data}->{post});
+
+        $message_args->{post_data} = $post_data;
 
         if ($post_data->{message} eq '!die') {
             $tx->finish(1010, 'Requested');
@@ -167,21 +196,18 @@ sub _on_message {
         #return if $post_data->{user_id} = $self->user_id;
     }
 
+    $self->emit(message => $message_args);
+
     if ($message->{event} eq 'hello') {
         if ($self->debug) {
             $self->logger->debug('Received "hello" event, sending authentication challenge');
         }
 
-        $output = {
+        $tx->send(encode_json({
             seq    => 1,
             action => 'authentication_challenge',
             data   => { token => $self->auth_token },
-        };
-    }
-
-    if ($output) {
-        # p $output;
-        $tx->send(encode_json($output));
+        }));
     }
 
     return 1;
@@ -195,13 +221,15 @@ sub _on_non_event {
         $self->logger->debugf('[Seq: %d] Received %s', $self->last_seq, $message->{data}->{text});
     }
 
-    return 1;
+    return $self->emit(message_no_event => $message);
 }
 
 sub _on_error {
     my $self    = shift;
     my $ws      = shift;
     my $message = shift;
+
+    $self->emit(error => { message => $message });
 
     return $ws->finish($message);
 }
@@ -234,11 +262,22 @@ sub _clean_up_loops {
 
 sub _build__ua { Mojo::UserAgent->new }
 
+sub _build_event_map {
+    return {
+        started          => 'gw_ws_started',
+        finished         => 'gw_ws_finished',
+        message          => 'gw_message',
+        error            => 'gw_error',
+        message_no_event => 'gw_message_no_event',
+    };
+}
+
 sub _build_ioloop { Mojo::IOLoop->singleton }
 
 sub _build_websocket_url {
     my $self = shift;
 
+    # Convert the API URL to the WebSocket URL
     my $ws_url = $self->base_url;
 
     if ($ws_url !~ /\/$/) {
@@ -254,4 +293,76 @@ sub _build_websocket_url {
 ################################################################################
 
 1;
+__END__
+
+=head1 NAME
+
+Net::Mattermost::WS::v4 - WebSocket gateway to Mattermost
+
+=head1 DESCRIPTION
+
+This class connects to Mattermost via the WebSocket gateway and can either be
+extended in a child class, or used in a script.
+
+=head2 USAGE
+
+=head3 FROM A SCRIPT
+
+    use Net::Mattermost::WS::v4;
+
+    my $bot = Net::Mattermost::WS::v4->new({
+        username => 'usernamehere',
+        password => 'password',
+        base_url => 'https://mattermost.server.com/api/v4/',
+
+        debug => 1, # optional
+    });
+
+    $bot->on(message => sub {
+        my ($bot, $args) = @_;
+
+        # $args contains the decoded message content
+    });
+
+    $bot->start(); # Add me last
+
+=head3 EXTENSION
+
+See C<Net::Mattermost::Example::Bot>.
+
+=head2 EVENTS
+
+Events are either available to be caught with C<on> in scripts, or have methods
+which can be overridden in child classes.
+
+=over 4
+
+=item C<started>
+
+The bot connected to the Mattermost gateway. Can be overridden as
+C<gw_ws_started()>.
+
+=item C<finished>
+
+The bot disconnected from the Mattermost gateway. Can be overridden as
+C<gw_ws_finished()>.
+
+=item C<message>
+
+The bot received a message. Can be overridden as C<gw_message()>.
+
+=item C<error>
+
+The bot received an error. Can be overridden as C<gw_error()>.
+
+=item C<message_no_event>
+
+The bot received a message without an event (which is usually a "ping" item).
+Can be overridden as C<gw_message_no_event()>.
+
+=back
+
+=head1 AUTHOR
+
+Mike Jones L<email:mike@netsplit.org.uk>
 
