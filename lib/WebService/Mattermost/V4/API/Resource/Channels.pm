80c88dad707a069520a4ef0011f7e9d5e64f83cc
Mike Jones <mike@netsplit.org.uk>
2018-09-22 16:09:29 +0100

    Remove stubs

--- lib/WebService/Mattermost/V4/API/Resource/Channels.pm
+++ lib/WebService/Mattermost/V4/API/Resource/Channels.pm
@@ -53,58 +53,6 @@ sub create_group_channel {
     });
 }
 
-sub update {
-    # PUT /{id}
-}
-
-sub delete {
-    # DELETE /{id}
-}
-
-sub patch {
-    # PUT /{id}/patch
-}
-
-sub convert {
-    # POST /{id}/convert
-}
-
-sub restore {
-    # POST /{id}/restore
-}
-
-sub stats {
-    # GET /{id}/stats
-}
-
-sub pinned {
-    # GET /{id}/pinned
-}
-
-sub members {
-    # GET /{id}/members
-}
-
-sub add_member {
-    # POST /{id}/members
-}
-
-sub members_by_ids {
-    # POST /{id}/members/ids
-}
-
-sub member_by_id {
-    # GET /{id}/members/{user_id}
-}
-
-sub remove_member {
-    # DELETE /{id}/members/{user_id}
-}
-
-sub update_roles {
-    # PUT /{id}/members/{user_ids}/roles
-}
-
 ################################################################################
 
 1;
