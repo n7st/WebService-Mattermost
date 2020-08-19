e4dd524bf23be54a66bd6449efa49d008da94f7e
Mike Jones <mike@netsplit.org.uk>
2020-08-19 08:07:45 +0100

    Bump version to 0.22

--- lib/WebService/Mattermost.pm
+++ lib/WebService/Mattermost.pm
@@ -5,7 +5,7 @@ use Types::Standard qw(Bool Int Object Str);
 
 use WebService::Mattermost::V4::API;
 
-our $VERSION = 0.20;
+our $VERSION = 0.22;
 
 ################################################################################
 
